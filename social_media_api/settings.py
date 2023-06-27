d203f0ecd37282e9fc46bcc731027815adcad706
Oleksandr Mamchur <irex1988@gmail.com>
2023-06-27 15:53:12 +0300

    Change registration credentials, email instead username

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "debug_toolbar",
     "user",
 ]
@@ -131,3 +132,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
--- user/serializers.py
+++ user/serializers.py
@@ -6,9 +6,12 @@ from rest_framework.exceptions import ValidationError
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        fields = ("id", "username", "email", "password", "is_staff")
+        read_only_fields = (
+            "id",
+            "is_staff",
+        )
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
