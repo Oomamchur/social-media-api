87286c4b6cd0c4f36e374387f386fc3a9e224035
Oleksandr Mamchur <irex1988@gmail.com>
2023-06-27 20:02:11 +0300

    Create logout view

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "user",
 ]
 
--- user/views.py
+++ user/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from user.serializers import UserSerializer, AuthTokenSerializer
 
@@ -21,3 +24,17 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request) -> Response:
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
