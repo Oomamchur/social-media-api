75b87aee9309be6fdcab20351e00978538ed3dc4
Oleksandr Mamchur <irex1988@gmail.com>
2023-06-29 15:08:22 +0300

    Add searching users by first name & last name

--- user/views.py
+++ user/views.py
@@ -7,6 +7,7 @@ from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from user.pagination import UserPagination
 from user.serializers import (
     UserSerializer,
     AuthTokenSerializer,
@@ -22,6 +23,19 @@ class CreateUserView(generics.CreateAPIView):
 class UserListView(generics.ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserListSerializer
+
+    def get_queryset(self) -> queryset:
+        queryset = super().get_queryset()
+
+        first_name = self.request.query_params.get("first_name")
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+
+        last_name = self.request.query_params.get("last_name")
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset.distinct()
 
 
 class UserDetailView(generics.RetrieveAPIView):
