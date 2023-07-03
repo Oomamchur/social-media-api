8bb087313d0990c80cefe62b25a093ac26426c0d
Oleksandr Mamchur <irex1988@gmail.com>
2023-07-03 21:29:00 +0300

    Make custom permission for PostViewSet

--- user/pagination.py
+++ user/pagination.py
@@ -5,3 +5,9 @@ class UserPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
+
+
+class PostPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
